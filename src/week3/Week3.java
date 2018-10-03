c4992a13d757f385abd4a9b5d2777da1714755a9
nducanh255 <43198097+nducanh255@users.noreply.github.com>
2018-10-03 15:49:37 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,15 +1,18 @@
-package week3;
 
 public class Week3 {
-
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if(m>n) return m;
+        return n;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int min = array[0];
+        for(int i=1 ; i<array.length ; i++){
+            if(min > array[i]) min=array[i];
+        }
+        return min;
     }
 
     /**
@@ -20,6 +23,15 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI = weight / (height*height);
+        if(BMI < 18.5) return "Thiếu cân";
+        if(BMI >= 18.5 && BMI < 23) return  "Bình thường";
+        if(BMI >= 23 && BMI < 25) return "Thừa cân";
+        return "Béo phì";
+    }
+
+    public static void main(String[] args){
+        double height=1.75,weight=70;
+        System.out.println(calculateBMI(weight,height));
     }
 }
